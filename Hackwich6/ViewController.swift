ec565fce254aef0d1d7fb8e360f7b6ab44b38169
CM Student <cmstudent@uhwo-acm-09.local>
2018-03-05 11:18:01 -1000

    completed parts 1 and 2

--- Hackwich6/ViewController.swift
+++ Hackwich6/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    
+    @IBOutlet var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderHasMoved(_ sender:Any) {
+        
+        print("The value of the slider is:\(slider.value)")
+        
+        currentValue = lroundf(slider.value)
+        
+        
+    }
+    
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+    
+        let message = "The value is: \(currentValue)"
+        
+        //1. create the alert view
+        let alert = UIAlertController(title:"Hello World", message:message, preferredStyle: .alert)
+        
+        //2. button that user taps to dismiss view controller
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        //3. add the button to the alerview
+        alert.addAction(action)
+    
+        //4. pressent alerview on the screen
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+        
+        
+    }
 
-}
 
